1342a94fae7fcee1bbe1e5dbfda212250d701513
Michelle Nygård <mican17@student.sdu.dk>
2025-08-21 14:00:52 +0200

    Added Common ground for the background

---
+++ CommonBackground/src/main/java/dk/sdu/cbse/BackgroundSPI.java
@@ -0,0 +1,9 @@
+package dk.sdu.cbse;
+
+import dk.sdu.cbse.common.data.GameData;
+
+import javax.swing.text.html.ImageView;
+
+public interface BackgroundSPI {
+    ImageView getBackground(GameData gamedata);
+}
