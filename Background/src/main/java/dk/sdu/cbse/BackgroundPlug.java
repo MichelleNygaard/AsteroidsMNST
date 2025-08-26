66185ce75b0b49439db44757481c51239393dda0
Michelle Nygård <mican17@student.sdu.dk>
2025-08-26 13:17:01 +0200

    Fixed background not showing

--- Background/src/main/java/dk/sdu/cbse/BackgroundPlug.java
+++ Background/src/main/java/dk/sdu/cbse/BackgroundPlug.java
@@ -17,18 +17,20 @@ public class BackgroundPlug implements IGamePluginService, BackgroundSPI {
 
     ImageView view;
     @Override
-    public void start(GameData gameData, World world){
+    public void start(GameData gameData, World world) {
 
     }
 
     @Override
-    public void stop(GameData gameData, World world){
+    public void stop(GameData gameData, World world) {
 
     }
 
     @Override
     public ImageView getBackground(GameData gameData) {
-        view = new ImageView(new Image(this.getClass().getResource("/images/spacebackground.png").toString()));
+
+        view = new ImageView(new Image(this.getClass().getResource("/images/pillars.png").toString()));
+
         view.setFitHeight(gameData.getDisplayHeight());
         view.setFitWidth(gameData.getDisplayWidth());
         return view;
--- Background/src/main/java/module-info.java
+++ Background/src/main/java/module-info.java
@@ -1,11 +1,14 @@
 import dk.sdu.cbse.BackgroundPlug;
 import javafx.scene.layout.Background;
+import dk.sdu.cbse.common.services.IGamePluginService;
+import dk.sdu.background.BackgroundSPI;
 
 module Background {
     requires Common;
     requires CommonBackground;
     requires spring.context;
     requires javafx.graphics;
-    provides dk.sdu.cbse.BackgroundPlug with dk.sdu.cbse.BackgroundPlug;
+    provides dk.sdu.background.BackgroundSPI with dk.sdu.cbse.BackgroundPlug;
+    provides IGamePluginService with dk.sdu.cbse.BackgroundPlug;
     exports dk.sdu.cbse;
 }
